a104cc15921214f38d334bc755b53e364ba6afa0
Kiyenya Kanstantsin <90129579+kienya@users.noreply.github.com>
2021-09-22 23:30:52 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let newArray = arr.reduce((array, item, i) => {
+    if (item === '--double-next' && item != arr[arr.length - 1]) {
+      array.push(arr[i + 1]);
+    } else if (item === '--double-prev' && item != arr[0]) {
+      array.push(arr[i - 1]);
+    } else if (item === '--discard-next' && item != arr[arr.length - 1]) {
+      arr[i + 1] = ''
+    } else if (item === '--discard-prev' && item != arr[0]) {
+      array.pop();
+    } else {
+      if (item != '--double-next' && item != '--double-prev' && item != '--discard-next' && item != '--discard-prev') {
+        array.push(item);
+      }
+    }
+    return array;
+  }, []).filter(item => item != '');
+  return newArray;
 }
