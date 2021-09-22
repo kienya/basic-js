640943b9312f4ac41c0287a19c791fa4683b5de7
Kiyenya Kanstantsin <90129579+kienya@users.noreply.github.com>
2021-09-22 23:38:52 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -17,17 +17,15 @@ export default function transform(arr) {
   if (!Array.isArray(arr)) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   }
-  if (!Array.isArray(arr)) {
-    throw new Error("'arr' parameter must be an instance of the Array!");
-  }
-  let newArray = arr.reduce((array, item, i) => {
-    if (item === '--double-next' && item != arr[arr.length - 1]) {
-      array.push(arr[i + 1]);
-    } else if (item === '--double-prev' && item != arr[0]) {
-      array.push(arr[i - 1]);
-    } else if (item === '--discard-next' && item != arr[arr.length - 1]) {
-      arr[i + 1] = ''
-    } else if (item === '--discard-prev' && item != arr[0]) {
+  let newArray = arr.slice(0);
+  newArray = newArray.reduce((array, item, i) => {
+    if (item === '--double-next' && item != newArray[newArray.length - 1]) {
+      array.push(newArray[i + 1]);
+    } else if (item === '--double-prev' && item != newArray[0]) {
+      array.push(newArray[i - 1]);
+    } else if (item === '--discard-next' && item != newArray[newArray.length - 1]) {
+      newArray[i + 1] = ''
+    } else if (item === '--discard-prev' && item != newArray[0]) {
       array.pop();
     } else {
       if (item != '--double-next' && item != '--double-prev' && item != '--discard-next' && item != '--discard-prev') {
